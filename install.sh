c0d34cc284f7abd1c28dfe39e93a310a05b7156d
Koterev Maksim <makoterev@gmail.com>
2021-03-12 19:38:17 +0300

    Updated install script and requirements

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt install -y python3-venv python3-pip
 python3 -m venv venv
 source venv/bin/activate
-pip install -U pip
-pip install -r requirements.txt
+pip install --no-cache-dir -U pip wheel
+pip install -r requirements.txt --no-cache-dir
